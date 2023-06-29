42573b9c2ae8d3f55432686fab0af1bdb44645d0
K4t3marina <marinaes.spbgti@gmail.com>
2023-06-29 19:31:25 +0400

    https://leetcode.com/problems/two-sum/

---
+++ 1.TwoSum.py
@@ -0,0 +1,10 @@
+nums = [3,3]
+target = 6
+r = []
+for i in range(len(nums)):
+    if (target-nums[i]) in nums:
+        if nums.index(target-nums[i]) > -1 and nums.index(target-nums[i]) != i:
+            r.append(i)
+            r.append(nums.index(target-nums[i]))
+            break
+print(r)
